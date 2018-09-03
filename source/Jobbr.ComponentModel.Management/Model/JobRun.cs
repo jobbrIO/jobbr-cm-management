95c9dfa0aa65e6b72014a6c86d4206dc296e3180
Oliver Zürcher <oliver.zuercher@gmail.com>
2018-09-03 13:41:00 +0200

    added more info to jobrun model

--- source/Jobbr.ComponentModel.Management/Model/JobRun.cs
+++ source/Jobbr.ComponentModel.Management/Model/JobRun.cs
@@ -6,7 +6,14 @@ namespace Jobbr.ComponentModel.Management.Model
     {
         public long Id { get; set; }
         public long JobId { get; set; }
+        public string JobName { get; set; }
+        public string JobType { get; set; }
         public long TriggerId { get; set; }
+        public string TriggerType { get; set; }
+        public string Definition { get; set; }
+        public string UserId { get; set; }
+        public string UserDisplayName { get; set; }
+        public string Comment { get; set; }
         public JobRunStates State { get; set; }
         public double? Progress { get; set; }
 
@@ -16,7 +23,6 @@ namespace Jobbr.ComponentModel.Management.Model
         public DateTime? EstimatedEndDateTimeUtc { get; set; }
 
         public string JobParameters { get; set; }
-
         public string InstanceParameters { get; set; }
     }
 }
