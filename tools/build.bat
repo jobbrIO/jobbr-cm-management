58f840b34f8390c43cb5dbb31dc38cc1c45ac955
Lukas Dürrenberger <lukas.duerrenberger@zuehlke.com>
2023-03-17 17:33:02 +0100

    Remove obsolete build and publish tooling

--- tools/build.bat
+++
@@ -1,3 +0,0 @@
-cd ..\source
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" Jobbr.ComponentModel.Management.sln /Build "Release|Any CPU"
-cd ..\tools
--- tools/create_nuget.bat
+++
@@ -1,1 +0,0 @@
-..\source\packages\NuGet.CommandLine.3.5.0\tools\NuGet.exe pack ..\source\Jobbr.ComponentModel.Management.nuspec -version %1
